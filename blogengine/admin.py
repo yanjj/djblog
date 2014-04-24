9f4c6548e0152aad2a7073eda3b37d0f973f7e96
vagrant <vagrant@precise64.(none)>
2014-04-24 09:48:21 +0000

    add blogengine

--- blogengine/admin.py
+++ blogengine/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+import models
+
+admin.site.register(models.Post)
 
 # Register your models here.
--- blogengine/tests.py
+++ blogengine/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
 from blogengine.models import Post
 
@@ -17,3 +17,46 @@ class PostTest(TestCase):
         self.assertEqual(len(all_posts), 1)
         only_post = all_posts[0]
         self.assertEqual(only_post, post)
+
+        self.assertEqual(only_post.title, 'My first post')
+
+
+class AdminTest(LiveServerTestCase):
+    def test_login(self):
+
+        c = Client()
+        response = c.get('/admin/')
+
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('Log in' in response.content)
+
+        c.login(username='yanjunjie', password="yanjunjie")
+
+        response = c.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+
+class AddPostTest(LiveServerTestCase):
+    def test_create_post(self):
+
+        c = Client()
+        response = c.get('/admin/blogengine/post/add/')
+
+        self.assertEquals(response.status_code, 200)
+
+        response = self.client.post('/admin/blogengine/post/add/', {
+            'title': 'My first post',
+            'text': 'This is my first post',
+            'pub_date_0': '2013-12-28',
+            'pub_date_1': '22:00:04'
+        },
+        follow=True
+        )
+
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('added successfully' in response.content)
+
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+
